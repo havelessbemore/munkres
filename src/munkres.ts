0d1e8394d55d3f7b45b7e91e3b43df90b70473f6
havelessbemore <dev.michael.rojas@gmail.com>
2024-03-25 11:07:51 -0400

    Replace class variables with input parameters

--- src/munkres.ts
+++ src/munkres.ts
@@ -6,29 +6,27 @@ import { toString } from "./utils/munkres";
 
 export class Munkres {
   protected mat: CostMatrix;
-  protected starY: number[];
+
   protected starX: number[];
 
   constructor(mat: CostMatrix) {
-    const X = mat[0].length;
-    const Y = mat.length;
-
+    const X = mat[0]?.length ?? 0;
     this.mat = copy(mat);
     this.starX = new Array(X).fill(-1);
-    this.starY = new Array(Y).fill(-1);
   }
 
   run(): [number, number][] {
-    this._step4();
+    this._step4(this.mat);
 
     // Get assignments
-    return this._getAssignments();
+    return this._getAssignments(this.starX);
   }
 
-  protected _steps1to3(): number {
-    const mat = this.mat;
-    const starX = this.starX;
-    const starY = this.starY;
+  protected _steps1to3(
+    mat: CostMatrix,
+    starX: number[],
+    starY: number[]
+  ): number {
     const X = starX.length;
     const Y = starY.length;
 
@@ -58,22 +56,21 @@ export class Munkres {
     return stars;
   }
 
-  protected _step4(): void {
-    const mat = this.mat;
+  protected _step4(mat: CostMatrix): void {
     const starX = this.starX;
-    const starY = this.starY;
     const X = starX.length;
-    const Y = starY.length;
+    const Y = mat.length;
+    const starY = new Array(Y).fill(-1);
     const primeY = new Array(Y).fill(-1);
 
-    let stars = this._steps1to3();
+    let stars = this._steps1to3(mat, starX, starY);
     while (stars < X) {
       // Find an uncovered zero
-      const [y, x] = this._findUncoveredZeroOrMin(primeY);
+      const [y, x] = this._findUncoveredZeroOrMin(mat, primeY, starX);
 
       // If not found
       if (mat[y][x] != 0) {
-        this._step6(mat[y][x], primeY);
+        this._step6(mat[y][x], mat, primeY, starX);
         continue;
       }
 
@@ -91,7 +88,7 @@ export class Munkres {
         --stars;
       } else {
         // Replace stars with primes and reset coverage
-        this._step5(y, x, primeY);
+        this._step5(y, x, primeY, starX, starY);
         primeY.fill(-1);
         ++stars;
       }
@@ -109,15 +106,21 @@ export class Munkres {
    * @param y - The initial prime's y coordinate.
    * @param x - The initial prime's x coordinate.
    * @param primeY - An array of prime y coordinates to x coordinates.
+   * @param starX - An array of star x coordinates to y coordinates.
+   * @param starY - An array of star y coordinates to x coordinates.
    */
-  protected _step5(y: number, x: number, primeY: number[]): void {
+  protected _step5(
+    y: number,
+    x: number,
+    primeY: number[],
+    starX: number[],
+    starY: number[]
+  ): void {
     // Sanity check
     if (primeY[y] != x) {
       throw new Error("Input must be prime.");
     }
 
-    const starX = this.starX;
-    const starY = this.starY;
     for (let py = starX[x]; py >= 0; py = starX[x]) {
       starX[x] = y;
       starY[y] = x;
@@ -128,9 +131,12 @@ export class Munkres {
     starY[y] = x;
   }
 
-  protected _step6(min: number, primeY: number[]): void {
-    const mat = this.mat;
-    const starX = this.starX;
+  protected _step6(
+    min: number,
+    mat: CostMatrix,
+    primeY: number[],
+    starX: number[]
+  ): void {
     const X = starX.length;
     const Y = primeY.length;
 
@@ -147,8 +153,7 @@ export class Munkres {
     }
   }
 
-  protected _getAssignments(): [number, number][] {
-    const starX = this.starX;
+  protected _getAssignments(starX: number[]): [number, number][] {
     const X = starX.length;
     const pairs: [number, number][] = new Array(X);
     for (let x = 0; x < X; ++x) {
@@ -157,9 +162,11 @@ export class Munkres {
     return pairs;
   }
 
-  protected _findUncoveredZeroOrMin(primeY: number[]): [number, number] {
-    const mat = this.mat;
-    const starX = this.starX;
+  protected _findUncoveredZeroOrMin(
+    mat: CostMatrix,
+    primeY: number[],
+    starX: number[]
+  ): [number, number] {
     const X = starX.length;
     const Y = primeY.length;
 
