ceacb491ace92cb85015865031e83f09e4c7d596
havelessbemore <dev.michael.rojas@gmail.com>
2024-04-03 10:28:26 -0400

    Add findMin() array function

--- src/utils/array.test.ts
+++ src/utils/array.test.ts
@@ -1,6 +1,76 @@
 import { describe, it, expect } from "vitest";
 
-import { getMin } from "./array";
+import { findMin, getMin } from "./array";
+
+describe(`${findMin.name}()`, () => {
+  it("returns undefined for an empty array", () => {
+    const array: number[] = [];
+    const result = findMin(array);
+    expect(result).toBeUndefined();
+  });
+
+  it("handles an array with a single element", () => {
+    const array = [42];
+    const result = findMin(array);
+    expect(result).toBe(0);
+  });
+
+  it("correctly identifies the minimum value when it is at the beginning of the array", () => {
+    const array = [1, 2, 3, 4, 5];
+    const result = findMin(array);
+    expect(result).toBe(0);
+  });
+
+  it("correctly identifies the minimum value when it is at the end of the array", () => {
+    const array = [2, 3, 4, 5, 1];
+    const result = findMin(array);
+    expect(result).toBe(4);
+  });
+
+  it("handles an array with all elements being the same value", () => {
+    const array = [2, 2, 2, 2, 2];
+    const result = findMin(array);
+    expect(result).toBe(0);
+  });
+
+  it("handles an array with negative values", () => {
+    const array = [-2, -3, -1, -5];
+    const result = findMin(array);
+    expect(result).toBe(3);
+  });
+
+  it("returns the minimum value from a non-empty array", () => {
+    const array = [5, 1, 4, 2, 3];
+    const result = findMin(array);
+    expect(result).toBe(1);
+  });
+
+  it("finds the minimum value in an array of bigints", () => {
+    expect(findMin([2n, 3n, 1n, 4n])).toBe(2);
+  });
+
+  it("finds the minimum value in an array of strings", () => {
+    expect(findMin(["b", "c", "a"])).toBe(2);
+  });
+
+  it("handles number example", () => {
+    const array = [3, 1, 2];
+    const result = findMin(array);
+    expect(result).toBe(1);
+  });
+
+  it("handles bigint example", () => {
+    const array = [3n, 1n, 2n];
+    const result = findMin(array);
+    expect(result).toBe(1);
+  });
+
+  it("handles string example", () => {
+    const array = ["d", "b", "c"];
+    const result = findMin(array);
+    expect(result).toBe(1);
+  });
+});
 
 describe(`${getMin.name}()`, () => {
   it("returns undefined for an empty array", () => {
--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,7 +1,53 @@
 /**
+ * Find the index of the minimum value in a given array.
+ *
+ * @param array - An array.
+ *
+ * @returns The zero-based index of the minimum value,
+ * or `undefined` if the array is empty.
+ *
+ * @example
+ * const array = [3, 1, 2];
+ * console.log(findMin(array)); // Output: 1
+ *
+ * @example
+ * const array = [3n, 1n, 2n];
+ * console.log(findMin(array)); // Output: 1
+ *
+ * @example
+ * const array = ['d', 'b', 'c'];
+ * console.log(findMin(array)); // Output: 1
+ */
+export function findMin(array: number[]): number | undefined;
+export function findMin(array: bigint[]): number | undefined;
+export function findMin(array: string[]): number | undefined;
+export function findMin<T extends number | bigint | string>(
+  array: T[]
+): number | undefined;
+export function findMin<T extends number | bigint | string>(
+  array: T[]
+): number | undefined {
+  const N = array.length;
+  if (N <= 0) {
+    return undefined;
+  }
+
+  let minI = 0;
+  let min = array[0];
+  for (let i = 1; i < N; ++i) {
+    if (min > array[i]) {
+      min = array[i];
+      minI = i;
+    }
+  }
+
+  return minI;
+}
+
+/**
  * Find the minimum value in a given array.
  *
- * @param array - An array of numbers.
+ * @param array - An array.
  *
  * @returns The minimum value, or `undefined` if the array is empty.
  *
