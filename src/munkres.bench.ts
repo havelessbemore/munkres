a2d5bffe1d0f42f6fae050d68cb27d006aba09b2
havelessbemore <dev.michael.rojas@gmail.com>
2024-04-03 10:32:10 -0400

    Add specific benchmarks for bigStep4() and step4()

--- src/munkres.bench.ts
+++ src/munkres.bench.ts
@@ -2,26 +2,9 @@ import { bench, describe } from "vitest";
 
 import { Matrix } from "./types/matrix";
 
+import { genMatrix } from "./utils/munkres.bench";
+
 import { munkres } from "./munkres";
-
-function genMatrix(
-  Y: number,
-  X: number,
-  minVal: number,
-  maxVal: number
-): Matrix<number> {
-  const costs: Matrix<number> = new Array(Y);
-  const spanV = maxVal - minVal;
-
-  for (let y = 0; y < Y; ++y) {
-    const row = new Array(X);
-    for (let x = 0; x < X; ++x) {
-      row[x] = minVal + Math.trunc(spanV * Math.random());
-    }
-    costs[y] = row;
-  }
-  return costs;
-}
 
 describe("munkres", () => {
   let mat: Matrix<number>;
---
+++ src/utils/munkres.bench.ts
@@ -0,0 +1,42 @@
+import { bench, describe } from "vitest";
+
+import { Matrix } from "../types/matrix";
+
+import { step4 } from "./munkres";
+
+export function genMatrix(
+  Y: number,
+  X: number,
+  minVal: number,
+  maxVal: number
+): Matrix<number> {
+  const costs: Matrix<number> = new Array(Y);
+  const spanV = maxVal - minVal;
+
+  for (let y = 0; y < Y; ++y) {
+    const row = new Array(X);
+    for (let x = 0; x < X; ++x) {
+      row[x] = minVal + Math.trunc(spanV * Math.random());
+    }
+    costs[y] = row;
+  }
+  return costs;
+}
+
+describe("munkres", () => {
+  let mat: Matrix<number>;
+  for (let i = 1; i < 10; ++i) {
+    const N = 1 << i;
+    bench(
+      `${N}x${N}`,
+      () => {
+        step4(mat);
+      },
+      {
+        setup: () => {
+          mat = genMatrix(N, N, -1e9, 1e9);
+        },
+      }
+    );
+  }
+});
