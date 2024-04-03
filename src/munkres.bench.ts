855460aaf0521bf3317f190fb1d61aefea0aad64
havelessbemore <dev.michael.rojas@gmail.com>
2024-04-03 10:08:10 -0400

    Update benchmark after noticing Infinity and zeros were unintentionally reflecting unrealistic runtimes

--- src/munkres.bench.ts
+++ src/munkres.bench.ts
@@ -1,27 +1,22 @@
 import { bench, describe } from "vitest";
 
-import { munkres } from "./munkres";
 import { Matrix } from "./types/matrix";
 
-const MIN_V = -1e9;
-const MAX_V = 1e9;
-function genMatrix(Y: number, X: number): Matrix<number> {
+import { munkres } from "./munkres";
+
+function genMatrix(
+  Y: number,
+  X: number,
+  minVal: number,
+  maxVal: number
+): Matrix<number> {
   const costs: Matrix<number> = new Array(Y);
-  const spanV = MAX_V - MIN_V;
+  const spanV = maxVal - minVal;
 
   for (let y = 0; y < Y; ++y) {
     const row = new Array(X);
     for (let x = 0; x < X; ++x) {
-      const r = Math.random();
-      if (r < 0.08) {
-        row[x] = -Infinity;
-      } else if (r > 0.92) {
-        row[x] = Infinity;
-      } else if (r > 0.46 && r < 0.54) {
-        row[x] = 0;
-      } else {
-        row[x] = MIN_V + Math.trunc(spanV * Math.random());
-      }
+      row[x] = minVal + Math.trunc(spanV * Math.random());
     }
     costs[y] = row;
   }
@@ -30,7 +25,7 @@ function genMatrix(Y: number, X: number): Matrix<number> {
 
 describe("munkres", () => {
   let mat: Matrix<number>;
-  for (let i = 3; i < 14; ++i) {
+  for (let i = 1; i < 10; ++i) {
     const N = 1 << i;
     bench(
       `${N}x${N}`,
@@ -39,7 +34,7 @@ describe("munkres", () => {
       },
       {
         setup: () => {
-          mat = genMatrix(N, N);
+          mat = genMatrix(N, N, -1e9, 1e9);
         },
       }
     );
