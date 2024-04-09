023a1d3b4b102074d046068430a742dccee67ebf
havelessbemore <dev.michael.rojas@gmail.com>
2024-04-09 12:15:23 -0400

    Update munkres benchmarks

--- src/munkres.bench.ts
+++ src/munkres.bench.ts
@@ -1,12 +1,12 @@
 import { bench, describe } from "vitest";
 
 import { munkres } from "./munkres";
-import { gen, map } from "./utils/matrix";
+import { gen } from "./utils/matrix";
 
 const VAL_MIN = -1e9;
 const VAL_MAX = 1e9;
 
-describe(`munkres`, () => {
+describe(`munkres | number`, () => {
   for (let i = 1; i < 10; ++i) {
     const N = 1 << i;
 
@@ -15,13 +15,23 @@ describe(`munkres`, () => {
       return VAL_MIN + Math.trunc(span * Math.random());
     });
 
-    bench(`${N}x${N} | number`, () => {
+    bench(`${N}x${N}`, () => {
       munkres(mat);
     });
+  }
+});
 
-    const bigMat = map(mat, v => BigInt(v));
-    bench(`${N}x${N} | bigint`, () => {
-      munkres(bigMat);
+describe(`munkres | bigint`, () => {
+  for (let i = 1; i < 12; ++i) {
+    const N = 1 << i;
+
+    const mat = gen(N, N, () => {
+      const span = VAL_MAX - VAL_MIN;
+      return BigInt(VAL_MIN + Math.trunc(span * Math.random()));
+    });
+
+    bench(`${N}x${N}`, () => {
+      munkres(mat);
     });
   }
 });
