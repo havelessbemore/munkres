44a21e980e537b61c481f300e0243ba0df1587e2
havelessbemore <dev.michael.rojas@gmail.com>
2024-04-10 10:29:45 -0400

    Update benchmark

--- src/munkres.bench.ts
+++ src/munkres.bench.ts
@@ -7,7 +7,7 @@ const VAL_MIN = -1e9;
 const VAL_MAX = 1e9;
 
 describe(`munkres | number`, () => {
-  for (let i = 1; i < 10; ++i) {
+  for (let i = 1; i < 13; ++i) {
     const N = 1 << i;
 
     const mat = gen(N, N, () => {
