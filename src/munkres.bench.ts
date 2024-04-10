324d95fdb361a7ec24ee06cfba00a3249ab382c8
havelessbemore <dev.michael.rojas@gmail.com>
2024-04-10 19:22:29 -0400

    Refactor benchmarks

--- src/munkres.bench.ts
+++ src/munkres.bench.ts
@@ -2,19 +2,21 @@ import { bench, describe } from "vitest";
 
 import { munkres } from "./munkres";
 import { gen } from "./utils/matrix";
+import { Matrix } from "./types/matrix";
 
-const VAL_MIN = -1e9;
-const VAL_MAX = 1e9;
+function genRandom(N: number): Matrix<number>;
+function genRandom(Y: number, X: number): Matrix<number>;
+function genRandom(Y: number, X = Y): Matrix<number> {
+  const minV = -1e9;
+  const maxV = 1e9;
+  const span = maxV - minV;
+  return gen(Y, X, () => minV + Math.trunc(span * Math.random()));
+}
 
 describe(`munkres | number`, () => {
   for (let i = 1; i < 13; ++i) {
     const N = 1 << i;
-
-    const mat = gen(N, N, () => {
-      const span = VAL_MAX - VAL_MIN;
-      return VAL_MIN + Math.trunc(span * Math.random());
-    });
-
+    const mat = genRandom(N);
     bench(`${N}x${N}`, () => {
       munkres(mat);
     });
@@ -24,12 +26,7 @@ describe(`munkres | number`, () => {
 describe(`munkres | bigint`, () => {
   for (let i = 1; i < 12; ++i) {
     const N = 1 << i;
-
-    const mat = gen(N, N, () => {
-      const span = VAL_MAX - VAL_MIN;
-      return BigInt(VAL_MIN + Math.trunc(span * Math.random()));
-    });
-
+    const mat = genRandom(N);
     bench(`${N}x${N}`, () => {
       munkres(mat);
     });
