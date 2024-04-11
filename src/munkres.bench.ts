1a5d3e424e780000e87a0ecb7f76ce974d30f606
havelessbemore <dev.michael.rojas@gmail.com>
2024-04-11 10:21:40 -0400

    Move benchmarking from vitest to tinybench

--- src/munkres.bench.ts
+++ src/munkres.bench.ts
@@ -1,34 +1,34 @@
-import { bench, describe } from "vitest";
+import { Bench } from "tinybench";
 
 import { munkres } from "./munkres";
-import { gen } from "./utils/matrix";
+import { gen, map } from "./utils/matrix";
 import { Matrix } from "./types/matrix";
+import { Suite } from "./utils/suite";
 
 function genRandom(N: number): Matrix<number>;
 function genRandom(Y: number, X: number): Matrix<number>;
 function genRandom(Y: number, X = Y): Matrix<number> {
-  const minV = -1e9;
-  const maxV = 1e9;
+  const minV = 1;
+  const maxV = 2e9;
   const span = maxV - minV;
   return gen(Y, X, () => minV + Math.trunc(span * Math.random()));
 }
 
-describe(`munkres | number`, () => {
-  for (let i = 1; i < 13; ++i) {
-    const N = 1 << i;
-    const mat = genRandom(N);
-    bench(`${N}x${N}`, () => {
-      munkres(mat);
-    });
-  }
-});
+const suite = new Suite();
+let bench: Bench;
 
-describe(`munkres | bigint`, () => {
-  for (let i = 1; i < 12; ++i) {
-    const N = 1 << i;
-    const mat = genRandom(N);
-    bench(`${N}x${N}`, () => {
-      munkres(mat);
-    });
-  }
-});
+suite.add(`number`, (bench = new Bench()));
+for (let i = 1; i < 13; ++i) {
+  const N = 1 << i;
+  const mat = genRandom(N);
+  bench.add(`${N}x${N}`, () => munkres(mat));
+}
+
+suite.add(`bigint`, (bench = new Bench()));
+for (let i = 1; i < 13; ++i) {
+  const N = 1 << i;
+  const mat = map(genRandom(N), v => BigInt(v));
+  bench.add(`${N}x${N}`, () => munkres(mat));
+}
+
+await suite.run();
