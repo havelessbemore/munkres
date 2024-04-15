8c016abc325ad0019dc13725c47c848da10c1cdf
havelessbemore <dev.michael.rojas@gmail.com>
2024-04-15 14:12:34 -0400

    Update ciReporter to output to file

--- benchmarks/utils/ciReporter.ts
+++ benchmarks/utils/ciReporter.ts
@@ -1,42 +1,43 @@
+import fs from "fs";
+
 import { Task } from "tinybench";
+
 import { BenchReporter } from "./benchReporter";
 
 export interface Result {
   name: string;
   unit: string;
   value: number;
+  range: number | string;
+  extra: string;
 }
 
 export class CIReporter implements BenchReporter {
-  protected isFirst: boolean;
+  protected outputPath: string;
+  protected results: Result[];
 
-  constructor() {
-    this.isFirst = true;
-  }
-
-  onStart(): void {
-    this.isFirst = true;
-    console.log("[");
-  }
-
-  onComplete(): void {
-    console.log("]");
+  constructor(outputPath: string) {
+    this.outputPath = outputPath;
+    this.results = [];
   }
 
   onTaskComplete(task: Task): void {
-    const samples = task.result!.samples.length;
-    const comma = this.isFirst ? "" : ",";
+    this.results.push(this._toResult(task));
+  }
 
-    this.isFirst = false;
-    console.log(
-      comma,
-      JSON.stringify({
-        name: task.name,
-        unit: "ms",
-        value: task.result!.mean,
-        range: task.result!.variance,
-        extra: `${samples} sample${samples === 1 ? "" : "s"}`,
-      })
-    );
+  onComplete(): void {
+    fs.appendFileSync(this.outputPath, JSON.stringify(this.results));
+  }
+
+  private _toResult(task: Task): Result {
+    const res = task.result!;
+    const samples = res.samples.length;
+    return {
+      name: task.name,
+      value: res.mean,
+      unit: "ms",
+      range: `±${res.rme.toFixed(2)}%`,
+      extra: `${samples} sample${samples === 1 ? "" : "s"}`,
+    };
   }
 }
