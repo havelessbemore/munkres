6c53896e06d76b4b0acd4eb80dfecc3974dbb39b
github-action-benchmark <github@users.noreply.github.com>
2024-04-15 16:19:55 +0000

    add Munkres Benchmarks (benchmarkjs) benchmark result for 6125a65e5d03064e651c4da7e53d964697d65eda

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1713196684379,
+  "lastUpdate": 1713197995090,
   "repoUrl": "https://github.com/havelessbemore/munkres",
   "entries": {
     "Munkres Benchmarks": [
@@ -36,6 +36,44 @@ window.BENCHMARK_DATA = {
             "name": "bigint[4096][4096]",
             "value": 0.12,
             "range": "±0.49%",
+            "unit": "ops/sec",
+            "extra": "10 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dev.michael.rojas@gmail.com",
+            "name": "havelessbemore",
+            "username": "havelessbemore"
+          },
+          "committer": {
+            "email": "dev.michael.rojas@gmail.com",
+            "name": "havelessbemore",
+            "username": "havelessbemore"
+          },
+          "distinct": true,
+          "id": "6125a65e5d03064e651c4da7e53d964697d65eda",
+          "message": "Update coverage to only run on main branch",
+          "timestamp": "2024-04-15T12:17:42-04:00",
+          "tree_id": "9680a61e40a58af79e3a0895d73a40a0b3c655de",
+          "url": "https://github.com/havelessbemore/munkres/commit/6125a65e5d03064e651c4da7e53d964697d65eda"
+        },
+        "date": 1713197994212,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "number[4096][4096]",
+            "value": 0.55,
+            "range": "±0.42%",
+            "unit": "ops/sec",
+            "extra": "10 samples"
+          },
+          {
+            "name": "bigint[4096][4096]",
+            "value": 0.13,
+            "range": "±0.84%",
             "unit": "ops/sec",
             "extra": "10 samples"
           }
