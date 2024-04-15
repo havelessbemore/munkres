155951bfaf374e703f3da084df7b11bf1cfc56ee
github-action-benchmark <github@users.noreply.github.com>
2024-04-15 15:11:12 +0000

    add number[][] Benchmark (benchmarkjs) benchmark result for 899c5a414d12afb0efd80aadc8497d4aebaef9e4

---
+++ dev/bench/data.js
@@ -0,0 +1,116 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1713193872154,
+  "repoUrl": "https://github.com/havelessbemore/munkres",
+  "entries": {
+    "number[][] Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "dev.michael.rojas@gmail.com",
+            "name": "havelessbemore",
+            "username": "havelessbemore"
+          },
+          "committer": {
+            "email": "dev.michael.rojas@gmail.com",
+            "name": "havelessbemore",
+            "username": "havelessbemore"
+          },
+          "distinct": true,
+          "id": "899c5a414d12afb0efd80aadc8497d4aebaef9e4",
+          "message": "Update benchmark workflow",
+          "timestamp": "2024-04-15T11:10:04-04:00",
+          "tree_id": "3bb19460ba9a9d9b9893ddb466f48594ef5dd83c",
+          "url": "https://github.com/havelessbemore/munkres/commit/899c5a414d12afb0efd80aadc8497d4aebaef9e4"
+        },
+        "date": 1713193871293,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "2x2",
+            "value": 2691649,
+            "range": "±0.82%",
+            "unit": "ops/sec",
+            "extra": "1345825 samples"
+          },
+          {
+            "name": "4x4",
+            "value": 1226960,
+            "range": "±0.53%",
+            "unit": "ops/sec",
+            "extra": "613480 samples"
+          },
+          {
+            "name": "8x8",
+            "value": 444975,
+            "range": "±0.27%",
+            "unit": "ops/sec",
+            "extra": "222488 samples"
+          },
+          {
+            "name": "16x16",
+            "value": 131992,
+            "range": "±0.32%",
+            "unit": "ops/sec",
+            "extra": "65997 samples"
+          },
+          {
+            "name": "32x32",
+            "value": 34285,
+            "range": "±0.85%",
+            "unit": "ops/sec",
+            "extra": "17143 samples"
+          },
+          {
+            "name": "64x64",
+            "value": 8636,
+            "range": "±0.46%",
+            "unit": "ops/sec",
+            "extra": "4318 samples"
+          },
+          {
+            "name": "128x128",
+            "value": 2007,
+            "range": "±0.82%",
+            "unit": "ops/sec",
+            "extra": "1004 samples"
+          },
+          {
+            "name": "256x256",
+            "value": 426,
+            "range": "±1.60%",
+            "unit": "ops/sec",
+            "extra": "214 samples"
+          },
+          {
+            "name": "512x512",
+            "value": 84.81,
+            "range": "±3.83%",
+            "unit": "ops/sec",
+            "extra": "43 samples"
+          },
+          {
+            "name": "1024x1024",
+            "value": 16.1,
+            "range": "±6.48%",
+            "unit": "ops/sec",
+            "extra": "10 samples"
+          },
+          {
+            "name": "2048x2048",
+            "value": 2.77,
+            "range": "±18.98%",
+            "unit": "ops/sec",
+            "extra": "10 samples"
+          },
+          {
+            "name": "4096x4096",
+            "value": 0.52,
+            "range": "±7.89%",
+            "unit": "ops/sec",
+            "extra": "10 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
