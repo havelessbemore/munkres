079cb423adc6d3be8d21f36712388c4f0e563e93
havelessbemore <dev.michael.rojas@gmail.com>
2024-05-10 14:16:25 -0400

    Update async benchmark

--- benchmarks/munkresAsync.bench.ts
+++ benchmarks/munkresAsync.bench.ts
@@ -4,8 +4,7 @@ import { Bench } from "tinybench";
 import workerpool from "workerpool";
 
 import type { Runner } from "../src/types/async";
-import type { MatrixLike } from "../src/types/matrixLike";
-import { munkresAsync } from "../src/munkres";
+import { munkres, munkresAsync } from "../src/munkres";
 import { gen } from "../src/utils/matrix";
 import { toTypedMatrix } from "../src/utils/matrixLike";
 import { randomInt } from "../src/utils/number";
@@ -13,60 +12,51 @@ import { randomInt } from "../src/utils/number";
 import { Suite } from "./utils/suite";
 import { TerminalReporter } from "./utils/terminalReporter";
 
+const MIN_VAL = 1;
+const MAX_VAL = Number.MAX_SAFE_INTEGER;
+
+export function genInt(): number {
+  return randomInt(MIN_VAL, MAX_VAL);
+}
+
 (async function main(): Promise<void> {
   // Create pool
   const pool = workerpool.pool("./examples/workerpool/worker.js", {
     maxWorkers: os.availableParallelism(),
   });
 
-  // Create matcher
-  const runner: Runner<unknown> = {
+  // Create runners
+  const runners: Runner<unknown>[] = [];
+  for (let n = 2; n < pool.maxWorkers!; n += n) {
+    runners.push({
+      size: n,
+      match: async (req) => await pool.exec("match", [req]),
+    });
+  }
+  runners.push({
     size: pool.maxWorkers!,
     match: async (req) => await pool.exec("match", [req]),
-  };
+  });
 
   let bench: Bench;
   const suite = new Suite({ warmup: true }).addReporter(new TerminalReporter());
 
-  suite.add(`number`, (bench = new Bench({ iterations: 50 })));
   for (let i = 1; i <= 12; ++i) {
     const N = 1 << i;
-    let mat: MatrixLike<number>;
-    bench.add(`${N}x${N}`, async () => await munkresAsync(mat, runner), {
-      beforeEach: () => {
-        mat = [];
-        mat = toTypedMatrix(gen(N, N, genInt));
-      },
-    });
-  }
-
-  suite.add(`bigint`, (bench = new Bench({ iterations: 50 })));
-  for (let i = 1; i <= 11; ++i) {
-    const N = 1 << i;
-    let mat: MatrixLike<bigint>;
-    bench.add(`${N}x${N}`, async () => await munkresAsync(mat, runner), {
-      beforeEach: () => {
-        mat = [];
-        mat = toTypedMatrix(gen(N, N, genBig));
-      },
-    });
+    const matSync = gen(N, N, genInt);
+    const matAsync = toTypedMatrix(matSync);
+    suite.add(`${N}x${N}`, (bench = new Bench({ iterations: 50 })));
+    bench.add(`sync`, () => munkres(matSync));
+    for (const runner of runners) {
+      bench.add(`async (${runner.size})`, async () => {
+        await munkresAsync(matAsync, runner);
+      });
+    }
   }
 
   try {
-    console.log(`THREADS: ${pool.maxWorkers}`);
     await suite.run();
   } finally {
     await pool.terminate();
   }
 })();
-
-const MIN_VAL = 1;
-const MAX_VAL = Number.MAX_SAFE_INTEGER;
-
-export function genBig(): bigint {
-  return BigInt(genInt());
-}
-
-export function genInt(): number {
-  return randomInt(MIN_VAL, MAX_VAL);
-}
